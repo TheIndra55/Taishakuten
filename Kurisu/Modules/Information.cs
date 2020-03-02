387371a2d47b5b44bbb3655534d54f6c8fb14a40
Indra <wuppie55@gmail.com>
2020-03-02 20:23:50 +0100

    Add guild info, user info and avatar commands

--- Kurisu/Modules/Information.cs
+++ Kurisu/Modules/Information.cs
@@ -1,7 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Text;
+using System.Linq;
 using System.Threading.Tasks;
 using DSharpPlus.CommandsNext;
 using DSharpPlus.CommandsNext.Attributes;
@@ -27,8 +26,52 @@ namespace Kurisu.Modules
             await ctx.RespondAsync("", false, embed);
         }
 
-        [Command("guild")]
+        [Command("guild"), Aliases("server", "serverinfo"), Description("Shows information about the current guild")]
         public async Task Guild(CommandContext ctx)
+        {
+            var guild = ctx.Guild;
+
+            var embed = new DiscordEmbedBuilder()
+                .WithTitle(guild.Name)
+                .WithDescription(guild.Id.ToString())
+                .AddField("Owner", guild.Owner.Username)
+                .AddField("Members", guild.MemberCount.ToString())
+                .AddField("Age", $"{guild.CreationTimestamp.Humanize()} ({guild.CreationTimestamp:g})")
+                .AddField("Region", guild.RegionId)
+                .WithThumbnailUrl(guild.IconUrl)
+                .Build();
+
+            await ctx.RespondAsync(embed: embed);
+        }
+
+        [Command("user"), Aliases("whois", "userinfo", "member"), Description("See info about a member")]
+        public async Task User(CommandContext ctx, DiscordMember user)
+        {
+            var embed = new DiscordEmbedBuilder()
+                .WithTitle($"{user.Username}#{user.Discriminator}")
+                .WithDescription(user.Id.ToString())
+                .AddField("Account creation", $"{user.CreationTimestamp.Humanize()} ({user.CreationTimestamp:g})")
+                .AddField("Guild join", $"{user.JoinedAt.Humanize()} ({user.JoinedAt:g})")
+                .AddField("Roles", string.Join(", ", user.Roles.Select(x => $"`{x.Name}`")))
+                .WithThumbnailUrl(user.AvatarUrl)
+                .Build();
+
+            await ctx.RespondAsync(embed: embed);
+        }
+
+        [Command("avatar"), Aliases("pf", "pic"), Description("Shows the user's avatar")]
+        public async Task Avatar(CommandContext ctx, DiscordUser user)
+        {
+            var embed = new DiscordEmbedBuilder()
+                .WithTitle($"Avatar of {user.Username}")
+                .WithImageUrl(user.AvatarUrl)
+                .Build();
+
+            await ctx.RespondAsync(embed: embed);
+        }
+
+        [Command("settings")]
+        public async Task Settings(CommandContext ctx)
         {
             await ctx.RespondAsync(JsonConvert.SerializeObject(Program.Guilds[ctx.Guild.Id]));
         }
