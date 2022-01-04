74aed4301916a7cac3b1a98c6dc6f45ec30a0999
Indra <wuppie55@gmail.com>
2022-01-04 18:30:55 +0100

    Initial commit

---
+++ Taishakuten/Commands/Info.cs
@@ -0,0 +1,26 @@
+﻿using DSharpPlus.Entities;
+using DSharpPlus.SlashCommands;
+using System.Threading.Tasks;
+
+namespace Taishakuten.Commands
+{
+    class Info : ApplicationCommandModule
+    {
+        [SlashCommand("avatar", "Get the avatar from a user")]
+        public async Task AvatarCommand(InteractionContext ctx, [Option("user", "The user to get the avatar from")] DiscordUser user = null)
+        {
+            if (user == null)
+            {
+                user = ctx.Guild == null ? ctx.User : ctx.Member;
+            }
+
+            var avatar = user is DiscordMember ? (user as DiscordMember).GuildAvatarUrl : user.AvatarUrl;
+
+            var embed = new DiscordEmbedBuilder()
+                .WithTitle($"Avatar of {user.Username}")
+                .WithImageUrl(avatar);
+
+            await ctx.CreateResponseAsync(embed);
+        }
+    }
+}
---
+++ Taishakuten/Program.cs
@@ -0,0 +1,32 @@
+﻿using DSharpPlus;
+using DSharpPlus.SlashCommands;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Taishakuten.Commands;
+
+namespace Taishakuten
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            await new Program().MainAsync(args);
+        }
+
+        public async Task MainAsync(string[] args)
+        {
+            var client = new DiscordClient(new DiscordConfiguration
+            {
+                Token = "",
+                MinimumLogLevel = LogLevel.Debug
+            });
+
+            var slash = client.UseSlashCommands();
+
+            slash.RegisterCommands<Info>(832001341865197579);
+
+            await client.ConnectAsync();
+            await Task.Delay(-1);
+        }
+    }
+}
